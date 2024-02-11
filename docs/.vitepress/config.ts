bf0da93728c3f745f34e905ec206e3b5490ad096
236sec <popole159za@gmail.com>
2024-02-11 10:32:15 +0700

    Add Mermaid

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vitepress'
-
+import { withMermaid } from "vitepress-plugin-mermaid";
 // https://vitepress.dev/reference/site-config
+
+export default withMermaid({
+  title: "Lseqz",
+  description: "a blog website using vitepress to render my content",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  },
+  mermaid:{
+    //mermaidConfig !theme here works for ligth mode since dark theme is forced in dark mode
+  },
+})
+/*
 export default defineConfig({
   title: "Lseqz",
   description: "a blog website using vitepress to render my content",
@@ -26,3 +56,4 @@ export default defineConfig({
     ]
   }
 })
+*/
